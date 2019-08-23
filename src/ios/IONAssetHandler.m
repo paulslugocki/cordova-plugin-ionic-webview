cd82e2faa6f34ce441419575a4ceee9d0fa72af8
Niklas Merz <nmerz@gedys-intraware.de>
2019-08-23 15:15:35 +0200

    Proxy body and header

--- src/ios/IONAssetHandler.m
+++ src/ios/IONAssetHandler.m
@@ -19,11 +19,14 @@
 
 - (void)webView:(WKWebView *)webView startURLSchemeTask:(id <WKURLSchemeTask>)urlSchemeTask
 {
+    Boolean loadFile = true;
     NSString * startPath = @"";
     NSURL * url = urlSchemeTask.request.URL;
+    NSDictionary * header = urlSchemeTask.request.allHTTPHeaderFields;
     NSString * stringToLoad = url.path;
     NSString * scheme = url.scheme;
     NSString * method = urlSchemeTask.request.HTTPMethod;
+    NSData * body = urlSchemeTask.request.HTTPBody;
     NSData * data;
     NSInteger statusCode;
 
@@ -31,23 +34,29 @@
         if ([stringToLoad hasPrefix:@"/_app_file_"]) {
             startPath = [stringToLoad stringByReplacingOccurrencesOfString:@"/_app_file_" withString:@""];
         } else if ([stringToLoad hasPrefix:@"/_http_proxy_"]||[stringToLoad hasPrefix:@"/_https_proxy_"]) {
+            loadFile = false;
             startPath = [stringToLoad stringByReplacingOccurrencesOfString:@"/_http_proxy_" withString:@"http://"];
-            startPath = [stringToLoad stringByReplacingOccurrencesOfString:@"/_https_proxy_" withString:@"https://"];
+            //startPath = [stringToLoad stringByReplacingOccurrencesOfString:@"/_https_proxy_" withString:@"https://"];
             NSLog(@"Proxy %@", startPath);
             NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
             [request setHTTPMethod:method];
             [request setURL:[NSURL URLWithString:startPath]];
-            [request setAllHTTPHeaderFields:[NSHTTPCookie requestHeaderFieldsWithCookies:[NSHTTPCookieStorage sharedHTTPCookieStorage].cookies]];
-            [request HTTPShouldHandleCookies];
-
-            NSError *error = nil;
-            NSHTTPURLResponse *responseCode = nil;
-
-            data = [NSURLConnection sendSynchronousRequest:request returningResponse:&responseCode error:&error];
-            statusCode = [responseCode statusCode];
-            if (!error) {
-                NSLog(@"%@", error);
+            if (body) {
+                [request setHTTPBody:body];
             }
+            [request setAllHTTPHeaderFields:header];
+            [request setAllHTTPHeaderFields:[NSHTTPCookie requestHeaderFieldsWithCookies:[NSHTTPCookieStorage sharedHTTPCookieStorage].cookies]];
+            //[request setHTTPShouldHandleCookies:YES];
+            
+            [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                if(error) {
+                    NSLog(@"Proxy error: %@", error);
+                }
+                
+                [urlSchemeTask didReceiveResponse:response];
+                [urlSchemeTask didReceiveData:data];
+                [urlSchemeTask didFinish];
+            }] resume];
         } else {
             startPath = self.basePath;
             if ([stringToLoad isEqualToString:@""] || [url.pathExtension isEqualToString:@""]) {
@@ -58,27 +67,26 @@
         }
     }
 
-    if(!data) {
+    if(loadFile) {
         data = [[NSData alloc] initWithContentsOfFile:startPath];
+        statusCode = 200;
+        if (!data) {
+            statusCode = 404;
+        }
+        NSURL * localUrl = [NSURL URLWithString:url.absoluteString];
+        NSString * mimeType = [self getMimeType:url.pathExtension];
+        id response = nil;
+        if (data && [self isMediaExtension:url.pathExtension]) {
+            response = [[NSURLResponse alloc] initWithURL:localUrl MIMEType:mimeType expectedContentLength:data.length textEncodingName:nil];
+        } else {
+            NSDictionary * headers = @{ @"Content-Type" : mimeType, @"Cache-Control": @"no-cache"};
+            response = [[NSHTTPURLResponse alloc] initWithURL:localUrl statusCode:statusCode HTTPVersion:nil headerFields:headers];
+        }
+        
+        [urlSchemeTask didReceiveResponse:response];
+        [urlSchemeTask didReceiveData:data];
+        [urlSchemeTask didFinish];
     }
-    statusCode = 200;
-    if (!data) {
-        statusCode = 404;
-    }
-    NSURL * localUrl = [NSURL URLWithString:url.absoluteString];
-    NSString * mimeType = [self getMimeType:url.pathExtension];
-    id response = nil;
-    if (data && [self isMediaExtension:url.pathExtension]) {
-        response = [[NSURLResponse alloc] initWithURL:localUrl MIMEType:mimeType expectedContentLength:data.length textEncodingName:nil];
-    } else {
-        NSDictionary * headers = @{ @"Content-Type" : mimeType, @"Cache-Control": @"no-cache"};
-        response = [[NSHTTPURLResponse alloc] initWithURL:localUrl statusCode:statusCode HTTPVersion:nil headerFields:headers];
-    }
-    
-    [urlSchemeTask didReceiveResponse:response];
-    [urlSchemeTask didReceiveData:data];
-    [urlSchemeTask didFinish];
-
 }
 
 - (void)webView:(nonnull WKWebView *)webView stopURLSchemeTask:(nonnull id<WKURLSchemeTask>)urlSchemeTask
